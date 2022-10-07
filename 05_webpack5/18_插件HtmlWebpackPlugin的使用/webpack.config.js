aecdef24be176fb11a29a1e3021ca6b25ddf1b23
sam lau <samlau458@gmail.com>
2022-10-08 00:02:08 +0800

    20221008

---
+++ 05_webpack5/18_插件HtmlWebpackPlugin的使用/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+function getAbsolutePath(filepath = "/build") {
+  return path.resolve(__dirname) + filepath;
+}
+
+console.log("11", getAbsolutePath());
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    // path: "D:coding\vue3-ts-note\05_webpack\05_webpack的基础配置", // 需要写绝对路径，windows下命令行输入pwd可获取。这里报错可能是因为有中文路径
+
+    path: getAbsolutePath(),
+    filename: "js/bundle.js",
+    // assetModuleFilename: "img/[name]_[hash:6][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ttf|woff2?)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "font/[name]_[hash:6][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
+};
---
+++ 05_webpack5/19_插件HtmlWebpackPlugin指定html模板打包以及webpack内置插件DefinePlugin的使用/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { DefinePlugin } = require("webpack");
+
+function getAbsolutePath(filepath = "/build") {
+  return path.resolve(__dirname) + filepath;
+}
+
+console.log("11", getAbsolutePath());
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    // path: "D:coding\vue3-ts-note\05_webpack\05_webpack的基础配置", // 需要写绝对路径，windows下命令行输入pwd可获取。这里报错可能是因为有中文路径
+
+    path: getAbsolutePath(),
+    filename: "js/bundle.js",
+    // assetModuleFilename: "img/[name]_[hash:6][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ttf|woff2?)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "font/[name]_[hash:6][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      title: "哈哈哈",
+    }),
+    new DefinePlugin({
+      BASE_URL: "'./'", // 以模板所在的路径（即public文件夹里面的index.html）为相对路径。这里要注意值应该是"'./'"
+    }),
+  ],
+};
