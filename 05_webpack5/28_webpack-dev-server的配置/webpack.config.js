9b4cac7bac7f8144320a09bd718cd109d8df6bcd
sam lau <samlau458@gmail.com>
2022-10-10 03:18:34 +0800

    20221010

---
+++ 05_webpack5/28_webpack-dev-server的配置/webpack.config.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { DefinePlugin } = require("webpack");
+const { VueLoaderPlugin } = require("vue-loader/dist/index");
+
+function getAbsolutePath(filepath = "/build") {
+  return path.resolve(__dirname) + filepath;
+}
+
+console.log("11", getAbsolutePath());
+
+module.exports = {
+  target: "web",
+  mode: "development",
+  devtool: "source-map",
+  // watch: true,
+  entry: "./src/index.js",
+  output: {
+    // path: "D:coding\vue3-ts-note\05_webpack\05_webpack的基础配置", // 需要写绝对路径，windows下命令行输入pwd可获取。这里报错可能是因为有中文路径
+
+    path: getAbsolutePath(),
+    filename: "js/bundle.js",
+    // assetModuleFilename: "img/[name]_[hash:6][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ttf|woff2?)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "font/[name]_[hash:6][ext]",
+        },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          // options: {
+          //   presets: ["@babel/preset-env"],
+          // },
+        },
+      },
+      {
+        test: /\.vue$/i,
+        use: ["vue-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      title: "哈哈哈",
+    }),
+    new DefinePlugin({
+      BASE_URL: "'./'",
+      __VUE_OPTIONS_API__: false, // 默认true
+      __VUE_PROD_DEVTOOLS__: true, // 默认false
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: "./",
+          globOptions: {
+            ignore: ["**/index.html"], // 忽略public文件夹下所有的index.html文件
+          },
+        },
+      ],
+    }),
+    new VueLoaderPlugin(),
+  ],
+  devServer: {
+    contentBase: "./public",
+    hot: true,
+  },
+};
